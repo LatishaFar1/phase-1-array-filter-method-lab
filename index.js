ffcedd23086e8157b1de4e8934ade790fdc8f017
Latisha Faroul <Lfaroul14@gmail.com>
2021-06-01 23:02:44 -0400

    commit tree eabfe0d70cfc93173cbc408258341e8323575776

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { arrayContaining } = require("expect")
+
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(driverArray, name){
+    return driverArray.filter(driverName => driverName.toLowerCase() === name.toLowerCase());
+};
+
+function fuzzyMatch(driverArray, name){
+    return driverArray.filter(person => name[0].toLowerCase() === person[0].toLowerCase())
+};
+
+function matchName(driverArray, name){
+    return driverArray.filter(person => person.name === name);
+
+}
